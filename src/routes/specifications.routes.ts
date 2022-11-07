b1463965642e0cfb39f6bce45af8fd60c1ec1a63
soupetersen <soupetersen@gmail.com>
2022-11-07 20:55:55 -0300

    Improve some routes

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 
-import { createSpecificationController } from "../modules/cars/useCases/createSpecification";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
+import { CreateSpecificationController } from "../modules/cars/useCases/createSpecification/CreateSpecificationController";
 
 export const specificationsRoutes = Router();
 
-specificationsRoutes.post("/", (request, response) => {
-    return createSpecificationController.handle(request, response);
-});
+specificationsRoutes.use(ensureAuthenticated);
+const createSpecificationController = new CreateSpecificationController();
+
+specificationsRoutes.post("/", createSpecificationController.handle);
